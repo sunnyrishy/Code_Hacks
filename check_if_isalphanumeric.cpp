e993243a8b26767ad0ac934acb9e19a053403a2d
Sunny Rishy Vardhan <sunnyrishyvardhan@gmail.com>
2025-10-14 12:00:26 -0400

    alphanumeric

---
+++ check_if_isalphanumeric.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main () {
+  string s = "I Am hello";
+  for(char c : s){
+    if(isalnum(c)){     //isalnum
+      //we will do something here based on the problem
+    }
+  }
+}
